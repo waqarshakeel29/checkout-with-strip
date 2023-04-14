6bfa861084b5d896de6fb270427bea69a2c42d61
Waqar Shakeel <waqarshakeel29@gmail.com>
2023-04-14 14:52:00 +0500

    checkout with strip

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/daoatdnsn/image/upload/v1681160424/online-shop/iphone12pro_oc5uzh.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/daoatdnsn/image/upload/v1681160422/online-shop/iphone12_qt5h5o.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/daoatdnsn/image/upload/v1681160424/online-shop/galaxyS_tmklfm.png",
+  },
+];
+
+module.exports = products;
